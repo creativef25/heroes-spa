93197c72e480360bd6060c11f449414314cb72b4
Israelf25 <desarrollo3@perzona.mx>
2025-04-23 13:43:46 -0600

    termino de la seccion 14

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -22,11 +22,11 @@ export const HeroCard = ({
 
 
     return (
-        <div className="col">
+        <div className="col animate__animated animate__fadeIn">
             <div className="card">
                 <div className="row no-gutters">
                     <div className="col-4">
-                        <img src={ heroImageUrl } className="card-img" alt={superhero}/>
+                        <img src={ heroImageUrl } className="card-img " alt={superhero}/>
                     </div>
 
                     <div className="col-8">
